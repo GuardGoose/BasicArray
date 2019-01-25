453ecad35b94ecb721d450ace724787ff1d0fd7f
Justin <justinszabo99@gmail.com>
2019-01-25 15:13:41 +0000

    Incremented the size of the list rather than the pointer, which fucked it up a bit

--- src/programming_problems/Lists.java
+++ src/programming_problems/Lists.java
@@ -1,26 +1,23 @@
 package programming_problems;
 
 public class Lists {
+	
 
-	public static void main(String[] args) {
-		int a[] = {12, 3, 54, 4, 2, 88, 23};
-		int b[] = {1, 23, 43, 23, 33, 44, 2};
-		double c[] = {0.2122, 0.3242, 0.2112};
-		
-
-	}
-
-	public static int Largest(int[] a, int total) {
+	public static int Largest(int[] a) {
 		// int[] a being the list and total being the size
 		int temp; // Temp value for counter
+		int total = a.length;  // Length of the list
 		
-		for(int i = 0; i < total; i++) {
+		for(int i = 0; i < total; i++) 
+		{
 			// j is the pointer
-			for(int j = i + 1; j < total; total++) {
+			for(int j = i + 1; j < total; j++) 
+			{
 				// if the value at a is greater than
 				// the value at j than j is the 
 				// new highest number
-				if(a[i] > a[j]) {
+				if(a[i] > a[j]) 
+				{
 					
 					temp = a[i];
 					a[i] = a[j];
@@ -29,8 +26,19 @@ public class Lists {
 			}
 		}
 		
-		return a[total -1];
+		return a[total - 1];
 	
+	}
+
+	public static void main(String[] args) {
+		int a[] = {12, 3, 54, 4, 2, 88, 23};
+		int b[] = {1, 23, 43, 23, 33, 44, 2};
+		int c[] = {23, 3, 1, 7777, 666, 45};
+		
+		System.out.println("Largest: " + Largest(a));
+		//System.out.println("Largest: " + Largest(b, 7));
+		//System.out.println("Largest: " + Largest(c, 6));
+
 	}
 	
 }
